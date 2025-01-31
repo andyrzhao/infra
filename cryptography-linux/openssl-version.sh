c17c1651a5ca44b8f89d9eee62aa8241f0fd492d
Andy Zhao <andyzhao@google.com>
2025-01-30 16:34:53 -0800

    Update openssl-version.sh to revert to original upstream settings

--- cryptography-linux/openssl-version.sh
+++ cryptography-linux/openssl-version.sh
@@ -2,5 +2,5 @@ export OPENSSL_VERSION="openssl-3.1.7"
 export OPENSSL_SHA256="053a31fa80cf4aebe1068c987d2ef1e44ce418881427c4464751ae800c31d06c"
 # We need a base set of flags because on Windows using MSVC
 # enable-ec_nistp_64_gcc_128 doesn't work since there's no 128-bit type
-export OPENSSL_BUILD_FLAGS_WINDOWS="no-ssl3 no-ssl3-method no-zlib no-shared no-module no-comp no-dynamic-engine"
+export OPENSSL_BUILD_FLAGS_WINDOWS="no-ssl3 no-ssl3-method no-zlib no-shared no-module no-comp no-dynamic-engine no-apps no-docs no-sm2-precomp no-atexit"
 export OPENSSL_BUILD_FLAGS="${OPENSSL_BUILD_FLAGS_WINDOWS} enable-ec_nistp_64_gcc_128"
